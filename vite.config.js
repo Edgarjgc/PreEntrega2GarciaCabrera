b56500d1516e1ca973fa869a272607ad63953b5b
edgarjgc <edgarjgarciac@gmail.com>
2023-11-26 13:58:24 -0500

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/PreEntrega1GarciaCabrera/",
   build: {
     outDir: "dist", // Specify the output directory
   },
